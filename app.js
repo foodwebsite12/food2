ceeaa402bdc15e6830a7026e3be30a20a875f39a
foodwebsite12 <143099632+foodwebsite12@users.noreply.github.com>
2023-10-24 05:15:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 let monu=document.querySelector("#home h1")
 
+let sonu=document.querySelector("#home .btn")
+
+
+
 setInterval(() => {
     monu.style.color = "red";
     monu.innerText="Welcome to Our Website";
+
+    sonu.style.backgroundColor = "red";
+
 }, 1000);
 
 setInterval(() => {
     monu.style.color = "blue";
     monu.innerText="Happy Durga Puja 🥰";
+
+    sonu.style.backgroundColor = "blue";
+
 }, 3000);
 
 setInterval(() => {
     monu.style.color = "green";
     monu.innerText="Great Offers Coming Soon";
+
+    sonu.style.backgroundColor = "green";
+
 }, 5000);
