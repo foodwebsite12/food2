3d50cfb784d1e763a634d7d86a2d1355fcfba40b
foodwebsite12 <143099632+foodwebsite12@users.noreply.github.com>
2023-10-27 09:45:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 let monu=document.querySelector("#home h1")
 
-let sonu=document.querySelector("#home .btn")
+let sonu=document.querySelector("#home button")
 
+let servicesDescription =document.querySelector("#services")
 
 
 setInterval(() => {
     monu.style.color = "red";
-    monu.innerText="Welcome to Our Website";
-
+    monu.innerText="Welcome to Our Website ✨🙏";
+    
     sonu.style.backgroundColor = "red";
 
+    servicesDescription.style.color="red";
 }, 1000);
 
 setInterval(() => {
@@ -18,12 +20,19 @@ setInterval(() => {
 
     sonu.style.backgroundColor = "blue";
 
+    servicesDescription.style.color="blue";
+
 }, 3000);
 
 setInterval(() => {
     monu.style.color = "green";
-    monu.innerText="Great Offers Coming Soon";
+    monu.innerText="Great Offers Coming Soon 🎁🎉✨";
 
     sonu.style.backgroundColor = "green";
 
+    servicesDescription.style.color="green";
+
 }, 5000);
+
+
+
